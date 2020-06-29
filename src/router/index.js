374f6f5f2cb1215864fdbbc3de8887e71960160e
Yutaka Okabe <yutaka.sh.6@gmail.com>
2020-06-29 23:12:26 +0900

    トップページのタイトルを作成

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,7 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // このコンポーネントはwebpackでまとめるときに別にしておいて，後からimportするという意味
   }
 ]
 
