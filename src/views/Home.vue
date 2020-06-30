4f44a6cbefbd92c82d60f27d39903e9486f540f3
Yutaka Okabe <yutaka.sh.6@gmail.com>
2020-06-30 23:22:03 +0900

    vuetifyの設定とアドレス帳の作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,12 +6,8 @@
 
 <script>
 // @ is an alias to /src
-import Addresses from '@/components/Addresses.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    Addresses
-  }
+  name: 'Home'
 }
 </script>
